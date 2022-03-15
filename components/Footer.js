30495cc7a538b6fd8d319ac2fafaf4e6c6da37db
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2022-03-15 14:25:52 -0300

    feat: ✨ view details pokemon

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import styles from "../styles/Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>PokeNext &copy; 2021</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Head from "next/head";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <title>PokeNext</title>
+      </Head>
+      <Navbar />
+      <main className="main-container">{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
